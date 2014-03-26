Fixing rotation derivatives

gbltst-hps.py
@@ -159,6 +159,8 @@ def exampleHpsTest(inputfile):
         uvDir = np.array([[-sinPhi, cosPhi, 0.], \
                             [-sinLambda * cosPhi, -sinLambda * sinPhi, cosLambda]])
         
+
+        if debug: print 'Track direction in curvilinear frame\n',uvDir
         
         # projection from  measurement to local (curvilinear uv) directions (duv/dm)
         proM2l = np.dot(uvDir, mDir.T)
@@ -254,16 +256,37 @@ def exampleHpsTest(inputfile):
         if( np.linalg.norm( tDirGlobal - strip.tDir) > 0.00001):
           print 'ERROR: tDirs are not consistent!'
           sys.exit(1)
+
+
+        # Projection matrix from tracking frame to measurement frame
+        # t_u = dot(u,i)*t_i + dot(u,j)*t_j + dot(u,k)*t_k
+        # where t_u is component in new u direction and t_i is in the i direction
+        prjTrkToMeas = np.array([strip.u,strip.v,strip.w])
+        if debug: 
+          print 'tDirGlobal ', tDirGlobal
+          print 'rotation matrix to meas frame\n', prjTrkToMeas
+          print 'tDirMeas ', tDirMeas_new
+          print 'normalMeas ', normalMeas_new
+          print 'tPosGlobal ', np.array( [strip.tPos]) , ' (origin ', np.array( [strip.origin] ),')'
+          print 'tDiff ', tDiff
+          print 'tPosMeas ', tPosMeas
+        # rotate to measurement frame          
+        tDirMeas = np.dot( prjTrkToMeas, tDirGlobal.T) 
+        normalMeas = np.dot( prjTrkToMeas, np.array([strip.w]).T )
+        # vector coplanar with measurement plane from origin to prediction
+        tDiff = np.array( [strip.tPos]) - np.array( [strip.origin] )
+        # rotate to measurement frame          
+        tPosMeas = np.dot( prjTrkToMeas, tDiff.T) 
+
         # rotate track direction to measurement frame          
-        tDirMeas = np.dot( tDirGlobal, np.array([strip.u, strip.v, strip.w]).T )
-        #tDirMeas = utils.rotateGlToMeas(strip,tDirGlobal)
-        normalMeas = np.dot( np.array([strip.w]) , np.array([strip.u, strip.v, strip.w]).T ) 
-        #normalMeas = utils.rotateGlToMeas(strip,strip.w) 
-        # non-measured directions 
+        # non-measured directions         
         vmeas = 0.
         wmeas = 0.
+
         # calculate and add derivatives to point
-        glDers = utils.globalDers(strip.layer,strip.meas,vmeas,wmeas,tDirMeas,normalMeas)
+        glDers = utils.globalDers(strip.layer,strip.meas,vmeas,wmeas,tDirMeas,tPosMeas,normalMeas)
+        if debug:
+          glDers.dump()
         ders = glDers.getDers(track.isTop())
         labGlobal = ders['labels']
         addDer = ders['ders']

utils.py
@@ -462,19 +462,20 @@ def gblSimpleJacobian(ds, cosl, bfac):
     return jac
 
 class globalDers:
-    def __init__(self,layer,umeas,vmeas,wmeas, tDir, normal):
+    def __init__(self,layer,umeas,vmeas,wmeas, tDir, tPred, normal):
         self.layer = layer # measurement direction
         self.umeas = umeas # measurement direction
         self.vmeas = vmeas # unmeasured direction
         self.wmeas = wmeas # normal to plane
         self.t = tDir # track direction
+        self.p = tPred # track prediction
         self.n = normal # normal to plane
         # Global derivaties of the local measurements
-        dm_dg = self.getMeasDers()
+        self.dm_dg = self.getMeasDers()
         # Derivatives of residuals w.r.t. measurement 
-        dr_dm = self.getResDers()
+        self.dr_dm = self.getResDers()
         # Derivatives of residuals w.r.t. global parameters
-        self.dr_dg = np.dot(dr_dm, dm_dg)
+        self.dr_dg = np.dot(self.dr_dm, self.dm_dg)
         #print 'dm_dg'
         #print dm_dg
         #print 'dr_dm'
@@ -482,6 +483,12 @@ class globalDers:
         #print 'dr_dg'
         #print self.dr_dg
     
+    def dump(self):
+        print 'globalDers:'
+        print 'layer ', self.layer
+        print 'umeas ', self.umeas, ' vmeas ', self.vmeas, ' wmeas ', self.wmeas
+        print 't ', self.t, ' p ', self.p, ' n ', self.n
+        print 'dm_dg\n',self.dm_dg, '\ndr_dm\n',self.dr_dm,'\ndr_dg\n',self.dr_dg
 
     def getDers(self,isTop):
         half_offset = 10000 
@@ -511,23 +518,24 @@ class globalDers:
 
     
     def getResDers(self):
-        # Derivatives of the local residual w.r.t. the measurements
-        tdotn = np.dot(self.t,self.n.T)
+        # Derivatives of the local perturbed residual w.r.t. the measurements m (u,v,w)'
+        tdotn = np.dot(self.t.T,self.n)
         drdg = np.eye(3)
-        #print self.t, self.n, tdotn, drdg
+        #print 't ', self.t, ' n ', self.n, ' dot(t,n) ', tdotn
         for i in range(3):
             for j in range(3):
                 delta = 0.
                 if i==j:
                     delta = 1.       
-                drdg[i][j] = delta - self.t[0][i]*self.n[0][j]/tdotn[0][0]
+                drdg[i][j] = delta - self.t[i]*self.n[j]/tdotn[0]
         return drdg
     
     
     
     
     def getMeasDers(self):
-        # Global derivaties of the local measurements
+        # Derivative of mt, the perturbed measured coordinate vector m 
+        # w.r.t. to global parameters: u,v,w,alpha,beta,gamma
 
         # Derivative of the local measurement for a translation in u
         dmu_du = 1.
@@ -543,15 +551,15 @@ class globalDers:
         dmw_dw = 1.
         # Derivative of the local measurement for a rotation around u-axis (alpha)
         dmu_dalpha = 0.
-        dmv_dalpha = self.wmeas
-        dmw_dalpha = -1.0 * self.vmeas
+        dmv_dalpha = self.p[2] # self.wmeas
+        dmw_dalpha = -1.0 * self.p[1] # -1.0 * self.vmeas
         # Derivative of the local measurement for a rotation around v-axis (beta)
-        dmu_dbeta = -1.0 * self.wmeas
+        dmu_dbeta = -1.0 * self.p[2] #-1.0 * self.wmeas
         dmv_dbeta = 0.
-        dmw_dbeta = self.umeas
+        dmw_dbeta = self.p[0] #self.umeas
         # Derivative of the local measurement for a rotation around w-axis (gamma)
-        dmu_dgamma = self.vmeas
-        dmv_dgamma = -1.0 * self.umeas
+        dmu_dgamma = self.p[1] # self.vmeas
+        dmv_dgamma = -1.0 * self.p[0]  # -1.0 * self.umeas 
         dmw_dgamma = 0.
         # put into matrix
         dmdg = np.array([[dmu_du, dmu_dv, dmu_dw, dmu_dalpha, dmu_dbeta, dmu_dgamma],[dmv_du, dmv_dv, dmv_dw, dmv_dalpha, dmv_dbeta, dmv_dgamma],[dmw_du, dmw_dv, dmw_dw, dmw_dalpha, dmw_dbeta, dmw_dgamma]])