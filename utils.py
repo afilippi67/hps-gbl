1a32889ee013efa60ec109a08b5233baa11a95dd
perhansson <per.hansson80@gmail.com>
2015-09-14 13:09:54 -0700

    Added helper function to get shorter sensor names

--- utils.py
+++ utils.py
@@ -921,3 +921,13 @@ def getCanvasIdxTwoCols(sensor):
     
     print sensor, " -> layer ", l, " / ", half, " / ", stereoname, " / ", side, "   ==>  ", i
     return i
+
+
+def getShortSensorName(name):
+    s = name
+    s = s.replace('_sensor0','')
+    s = s.replace('_halfmodule','')
+    s = s.replace('_module','')
+    return s
+
+    
