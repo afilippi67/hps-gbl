6441eca66e0e290eeb0f7e7ac42ac4a60bc363de
perhansson <per.hansson80@gmail.com>
2015-09-14 13:10:20 -0700

    Add graphs of mean and sigma to sensor plots

--- cmpGblPlots.py
+++ cmpGblPlots.py
@@ -2,14 +2,15 @@
 
 import sys, os, re
 import argparse
-from ROOT import TFile, TH1F, TF1, TCanvas, gDirectory, TGraph
-#sys.path.append('pythonutils')
+from ROOT import TFile, TH1F, TF1, TCanvas, gPad, gDirectory, TGraph, TGraphErrors
+sys.path.append('pythonutils')
 import utils
 import compareRootHists 
+import plotutils
 
 def getArgs():
     parser = argparse.ArgumentParser(description='Run comparison')
-    parser.add_argument('--file','-f',nargs='+',help='Input files.')
+    parser.add_argument('--file','-f',nargs='+', required=True,help='Input files.')
     parser.add_argument('--tag','-t',default='',help='Tag to output files.')
     args = parser.parse_args();
     print args
@@ -58,6 +59,19 @@ def compareSensorHists(files,histName,tag='',half=None,singleCanvas=None):
         c = TCanvas(cName, cName, 10, 10, 690*2, 390*2)
         c.Divide(2,12)
     currentPad = None
+
+    graphBinNames = {}
+    graphMean = []
+    graphRMS = []
+    for tf in tFiles:
+        print 'tf ', tf.GetName()
+        grM = TGraphErrors()
+        grM.SetName('grMean_' + tf.GetName())
+        grR = TGraphErrors()
+        grR.SetName('grRMS_' + tf.GetName())
+        graphMean.append( grM )
+        graphRMS.append( grR )
+    
     for hName in sensorHistNames:
         histos = []
         print 'get ', hName
@@ -65,22 +79,94 @@ def compareSensorHists(files,histName,tag='',half=None,singleCanvas=None):
             print 'tf ', tf.GetName()
             h = tf.Get(hName)
             histos.append(h)
+            
         print 'compare ', len(histos),' root histos'
         if c != None:
             i = utils.getCanvasIdxTwoCols(hName)
             currentPad = c.cd(i)
         print currentPad
         compareRootHists.compareHists(histos,legends=None,normalize=None,fitName='gaus',t=tag,pad=currentPad)
+        print 'make graphs of mean and RMS'
+        for ih in range(len(histos)):
+            h = histos[ih]
+            f = h.GetFunction('gaus')
+            if f != None:
+                mean = f.GetParameter(1)
+                meanError = f.GetParError(1)
+                rms = f.GetParameter(2)
+                rmsError = f.GetParError(2)
+                ipoint = graphMean[ih].GetN()
+                graphMean[ih].SetPoint(ipoint, ipoint, mean)
+                graphRMS[ih].SetPoint(ipoint, ipoint, rms)
+                graphMean[ih].SetPointError(ipoint, 0., meanError)
+                graphRMS[ih].SetPointError(ipoint, 0., rmsError)
+                graphBinNames[ ipoint ] = utils.getShortSensorName( hName )
+                #print 'mean ', mean, ' RMS ', rms
+        
         print 'done comparing ', len(histos),' root histos'
     if c != None:
         c.SaveAs(cName + '.png')
+
+    cGrName = 'c_summary' + histName + '_' + half + '_' + tag 
+    cGr = TCanvas(cGrName, cGrName, 10, 10, 690*2, 390*2)
+    cGr.Divide(1,2)
+    grMaxValMean = -1000000.
+    grMaxValRMS = -10000000.
+    grMinValMean = 1000000.
+    grMinValRMS = 10000000.
+    
+    for igr in range(len(graphMean)):
+        grM = graphMean[ igr ]
+        grR = graphRMS [ igr ]
+        limVals = plotutils.getGraphMaxMinVal(grM)
+        print limVals
+        if limVals[1] > grMaxValMean:
+            grMaxValMean = limVals[1]
+        if limVals[0] < grMinValMean:
+            grMinValMean = limVals[0]
+        limVals = plotutils.getGraphMaxMinVal(grR)
+        if limVals[1] > grMaxValRMS:
+            grMaxValRMS = limVals[1]
+        if limVals[0] < grMinValMean:
+            grMinValRMS = limVals[0]
+
+    print grMaxValMean, grMinValMean
+    
+    for igr in range(len(graphMean)):
+        grM = graphMean[ igr ]
+        grR = graphRMS [ igr ]
+        #print 'igr ', igr, ' name ', grM.GetName() 
+        plotutils.setGraphStyle(grM,igr+1)
+        plotutils.setGraphStyle(grR,igr+1)
+        plotutils.setBinLabelsDict(grM,graphBinNames)
+        plotutils.setBinLabelsDict(grR,graphBinNames)
+        if igr == 0:
+            cGr.cd(1)
+            gPad.SetBottomMargin(0.3)
+            grM.GetHistogram().SetMaximum(grMaxValMean*1.2)
+            grM.GetHistogram().SetMinimum(grMinValMean*1.2)
+            grM.Draw('AXPL')
+            cGr.cd(2)
+            gPad.SetBottomMargin(0.3)
+            grR.SetMaximum(grMaxValRMS)
+            grR.SetMinimum(grMinValRMS)
+            grR.Draw('AXPL')
+        else:
+            cGr.cd(1)
+            grM.Draw('PL,same')
+            cGr.cd(2)
+            grR.Draw('PL,same')
+    cGr.SaveAs(cGr.GetName() + '.png')
+
+    #ans = raw_input('press anything')
+    
+        
     for tf in tFiles:        
         print 'closing ', tf.GetName()
         tf.Close()
     return
 
     
-
 
 def compareGblHists(files,tag):
 
